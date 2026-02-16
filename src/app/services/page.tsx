0489c040eec512fc918d75a0c5effc483cf906c6
Remotized IT <remotizedit@gmail.com>
2026-02-16 15:36:41 +0000

    service banner image changed

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -72,7 +72,7 @@ const PageHeader = () => (
     <AnimatedContent distance={50}>
         <div className="relative py-20 lg:py-32 text-white">
             <Image
-                src="https://res.cloudinary.com/dyp8op8ov/image/upload/v1771227294/Alight_Ability_December_ztfqyu.png"
+                src="https://res.cloudinary.com/dyp8op8ov/image/upload/f_auto,q_auto,w_auto/v1771249940/Frame_945_s3h1iq.png"
                 alt="Alight Ability Services Banner"
                 fill
                 className="object-cover"
