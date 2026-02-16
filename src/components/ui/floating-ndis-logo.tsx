7dfe6a967bca3c7eae6e31cda42e5d195a8bceed
Remotized IT <remotizedit@gmail.com>
2026-02-16 11:18:53 +0000

    make it a little bit bigger 30% and set it down but right abobe of the m

--- src/components/ui/floating-ndis-logo.tsx
+++ src/components/ui/floating-ndis-logo.tsx
@@ -24,11 +24,11 @@ const FloatingNdisLogo = () => {
 
   return (
     <Link href="https://www.ndis.gov.au/" target="_blank" rel="noopener noreferrer" className={cn(
-        'fixed top-4 right-4 z-50 transition-all duration-500 ease-in-out',
-        isScrolled ? 'w-20 h-20' : 'w-28 h-28 md:w-32 md:h-32'
+        'fixed top-[30vh] right-4 z-50 transition-all duration-500 ease-in-out',
+        isScrolled ? 'w-24 h-24' : 'w-36 h-36 md:w-40 md:h-40'
     )}>
         <Image
-            src="https://res.cloudinary.com/dyp8op8ov/image/upload/f_auto,q_auto,w_auto/NDIS_y9urkg.png"
+            src="https://res.cloudinary.com/dyp8op8ov/image/upload/f_auto,q_auto,w_auto/v1771240431/NDIS_y9urkg.png"
             alt="NDIS Registered Provider"
             fill
             className="object-contain"
